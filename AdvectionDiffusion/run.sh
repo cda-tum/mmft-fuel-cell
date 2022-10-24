98dddceb22c2920250641ea85a31c75aa06f0502
micheltakken <micheltakken@gmail.com>
2022-10-24 21:45:41 +0200

    Adapt run.sh with correct stl path

--- AdvectionDiffusion/run.sh
+++ AdvectionDiffusion/run.sh
@@ -5,4 +5,4 @@ N=$2
 if [ ! -d "./tmpCube/" ]; then
     mkdir ./tmpCube/
 fi
-mpirun -np ${nproc} ./AdvectionDiffusion ${N} ChannelGeometries/Cube.stl ./tmpCube/ &> Cube.out
+mpirun -np ${nproc} ./AdvectionDiffusion ${N} ../ChannelGeometries/Cube.stl ./tmpCube/ &> Cube.out
